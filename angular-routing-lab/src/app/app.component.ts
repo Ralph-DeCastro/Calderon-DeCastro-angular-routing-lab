199cd0e6391d6ad57c818c4240d238b929081adc
Ralph-DeCastro <20237908@s.ubaguio.edu>
2025-03-06 16:30:57 +0800

    added home about profile
    edited some content in our project

--- angular-routing-lab/src/app/app.component.ts
+++ angular-routing-lab/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ProfileComponent } from './about/profile/profile.component';
 import { ContactComponent } from './contact/contact.component';
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AboutComponent,ProfileComponent,ContactComponent,HomeComponent],
+  imports: [RouterOutlet,AboutComponent,ProfileComponent,ContactComponent,HomeComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
