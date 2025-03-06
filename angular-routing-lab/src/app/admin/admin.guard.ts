be9f88c2fd8129bf1a5fbae8e1664ae40d3bb2a3
Ralph-DeCastro <20237908@s.ubaguio.edu>
2025-03-06 15:23:44 +0800

    Update admin.guard.ts
    editing admin routes so that it will work

--- angular-routing-lab/src/app/admin/admin.guard.ts
+++ angular-routing-lab/src/app/admin/admin.guard.ts
@@ -15,15 +15,5 @@ export class AdminGuard implements CanActivate {
   }
 }
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  {
-    path: 'admin',
-    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), // Lazy load AdminModule
-    canActivate: [AdminGuard], // Protect this route with AdminGuard
-  }
-];
+
 
