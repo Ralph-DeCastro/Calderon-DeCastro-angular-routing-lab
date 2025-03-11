a5f8d1a49b460bf1a6c5a488ec7438cd56a6b399
Ralph-DeCastro <20237908@s.ubaguio.edu>
2025-03-11 16:43:52 +0800

    Created by de castro and calderon
    We created our projects router link and design

--- angular-routing-lab/src/app/app-routing.module.ts
+++ angular-routing-lab/src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { AdminGuard } from './admin/admin.guard'; // Import the AdminGuard
 import { HomeComponent } from './home/home.component'; // Make sure to import components
 import { AboutComponent } from './about/about.component'; // Similarly, import other components
 import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './about/profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'about', component: AboutComponent, children: [
+    {path: 'profile', component: ProfileComponent}
+  ]},
   { path: 'contact', component: ContactComponent },
   {
     path: 'admin',
--- angular-routing-lab/src/app/app.component.ts
+++ angular-routing-lab/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ProfileComponent } from './about/profile/profile.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AboutComponent,ProfileComponent,ContactComponent,HomeComponent,RouterLink,RouterLinkActive],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
