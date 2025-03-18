81b32680e3d3a2648ebcd1ad68e0fa27ab33e176
UBSIT_Calderon <20237191@s.ubaguio.edu>
2025-03-18 16:23:16 +0800

    modal
    added some modal and other feature

--- angular-child-routes-example/src/app/app.component.ts
+++ angular-child-routes-example/src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink,RouterLinkActive],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-child-routes-example';
+  name = '';
+  email = '';
+  age = '';
+  religion = '';
+  
+  
+  isModalVisible: boolean = false;
+
+  openModal(): void {
+    this.isModalVisible = true;
+  }
+
+  closeModal(): void {
+    this.isModalVisible = false;
+  }
+  
 }
