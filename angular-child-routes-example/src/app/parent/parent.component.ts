ade876a9409d65fbf1eb443bc3c67cb092a4a9a2
UBSIT_Calderon <20237191@s.ubaguio.edu>
2025-03-18 14:05:52 +0800

    added some router outlet
    added some router outlet in the app component.html

--- angular-child-routes-example/src/app/parent/parent.component.ts
+++ angular-child-routes-example/src/app/parent/parent.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';  // Import RouterModule
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';  // Import RouterModule
 
 @Component({
   selector: 'app-parent',
