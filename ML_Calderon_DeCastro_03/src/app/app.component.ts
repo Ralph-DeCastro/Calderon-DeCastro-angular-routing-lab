53a413f638e2ce1fdb4aee7331244b4d2cc5ef14
UBSIT_Calderon <20237191@s.ubaguio.edu>
2025-03-20 16:54:17 +0800

    added some components
    created the auth component and designed the login component

--- ML_Calderon_DeCastro_03/src/app/app.component.ts
+++ ML_Calderon_DeCastro_03/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @Component({
   selector: 'app-root',
